1d2d5644b5043f7e68899ab8eb5c49a9e8e0ceb9
Anzo52 <andy@zollnersolutions.com>
2024-01-14 23:00:07 -0700

    Add *.jpg to .gitignore

---
+++ check4match.py
@@ -0,0 +1,44 @@
+import face_recognition
+
+
+def check4match(face1_emcoding, face2_encoding):
+    # compare the face encodings using compare_faces
+    results = face_recognition.compare_faces([face1_emcoding], face2_encoding)
+
+    # if array of encodings is passed as first parameter
+    # then just compare the first encoding in the array
+    if results[0] == True:
+        print("It's a match!")
+        return True
+    else:
+        print("It's not a match!")
+        return False
+
+
+def load_image():
+    image_path = input("Enter the path of the image: ")
+    image = face_recognition.load_image_file(image_path)
+    return image
+
+
+def get_encoding(image):
+    # get the face encoding of the image
+    face_encoding = face_recognition.face_encodings(image)[0]
+    return face_encoding
+
+
+def main():
+    # load the image
+    image1 = load_image()
+    image2 = load_image()
+
+    # get the face encoding of the image
+    face1_encoding = get_encoding(image1)
+    face2_encoding = get_encoding(image2)
+
+    # compare the face encodings using compare_faces
+    check4match(face1_encoding, face2_encoding)
+
+
+if __name__ == "__main__":
+    main()
